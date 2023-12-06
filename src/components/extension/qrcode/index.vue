1cf582cfe660a3b9afc19daf3f1b444d55d71ffa
WangYuncheng <w729567588@163.com>
2023-12-06 13:26:16 +0800

    feat: qrcode

--- src/components/extension/qrcode/index.vue
+++ src/components/extension/qrcode/index.vue
@@ -1,9 +1,47 @@
 <script lang="ts" setup>
-defineProps<{
-  mode: 'svg' | 'string' | 'file'
-}>()
+import QRCode, { QRCodeToDataURLOptions } from 'qrcode'
+
+const props = withDefaults(
+  defineProps<{
+    mode?: 'canvas' | 'image'
+    content: string
+    width?: number
+    options?: QRCodeToDataURLOptions
+  }>(),
+  {
+    mode: 'canvas',
+    width: 128
+  }
+)
+
+const fileSrc = ref<string>()
+const qrcodeRef = shallowRef<string>()
+
+onMounted(() => {
+  if (props.mode === 'canvas') {
+    QRCode.toCanvas(qrcodeRef.value, props.content, (err) => {
+      if (err) console.error(err)
+    })
+  } else if (props.mode === 'image') {
+    QRCode.toDataURL(
+      props.content,
+      {
+        ...props.options,
+        errorCorrectionLevel: 'H'
+      },
+      (err, url) => {
+        if (err) console.error(err)
+        console.log(url)
+        fileSrc.value = url
+      }
+    )
+  }
+})
 </script>
 
-<template></template>
+<template>
+  <canvas v-if="mode === 'canvas'" ref="qrcodeRef" :class="`!h-[${width}px] !w-[${width}px]`" />
+  <img v-if="mode === 'image'" :src="fileSrc" alt="qrcode" :class="`h-[${width}px]`" />
+</template>
 
 <style scoped lang="scss"></style>
--- src/views/extensions/qrcode/index.vue
+++ src/views/extensions/qrcode/index.vue
@@ -9,8 +9,35 @@
         <a href="https://www.npmjs.com/package/qrcode" target="_blank"> https://www.npmjs.com/package/qrcode </a>
       </template>
     </a-alert>
-    <regular-layout>
-      <qrcode />
+    <regular-layout class="flex">
+      <a-card>
+        <a-card-grid class="text-center">
+          <div class="mb-2">image模式</div>
+          <qrcode content="https://dcwedu.top" mode="image" />
+        </a-card-grid>
+        <a-card-grid class="text-center">
+          <div class="mb-2">canvas模式</div>
+          <qrcode content="https://dcwedu.top" mode="canvas" />
+        </a-card-grid>
+        <a-card-grid class="text-center">
+          <div class="mb-2">设置宽度</div>
+          <qrcode content="https://dcwedu.top" mode="canvas" :width="100" />
+        </a-card-grid>
+        <a-card-grid class="text-center">
+          <div class="mb-2">自定义二维码生成选项（仅对image模式生效）</div>
+          <qrcode
+            content="https://dcwedu.top"
+            mode="image"
+            :options="{
+              errorCorrectionLevel: 'M',
+              color: {
+                dark: '#194785',
+                light: '#eeeeee'
+              }
+            }"
+          />
+        </a-card-grid>
+      </a-card>
     </regular-layout>
   </div>
 </template>
